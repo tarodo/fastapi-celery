ac5a81e8150ba4d5389a98d8b1b9d09031cbd123
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-07-06 10:47:04 +0300

    blocking processes

--- project/users/tasks.py
+++ project/users/tasks.py
@@ -6,3 +6,10 @@ def divide(x, y):
     import time
     time.sleep(5)
     return x / y
+
+
+@shared_task()
+def sample_task(email):
+    from project.users.views import api_call
+
+    api_call(email)
